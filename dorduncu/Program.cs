d4ee5c201c7e8c60fcd8149403251a0e4ed483e1
lamanpoladli <lamanpoladli@gmail.com>
2023-02-28 23:47:42 +0400

    cc

---
+++ dorduncu/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("ededi daxil edin:");
+string b = Console.ReadLine();
+int a = int.Parse(b);
+int counter = 0;
+int cem = 0;
+for (int i = 1; i <= a; i++)
+{
+    for(int j=1; j <= i; j++)
+    {
+        if (i % j == 0)
+            counter++;
+        
+    }
+    if (counter <= 2)
+        cem = cem + i;
+}
+Console.WriteLine(cem);
